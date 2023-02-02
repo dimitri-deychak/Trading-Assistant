c96fadc15b97390c867b53afbf069fbdd3ffd045
dimitri-deychak <dimitrid@attentivemobile.com>
2023-02-01 19:45:35 -0600

    fix clock

--- src/server/listener/clock.ts
+++ src/server/listener/clock.ts
@@ -1,4 +1,3 @@
-import { getCST } from '../../shared/utils';
 import { clock } from './intervals';
 
 export const isWithinOneMinuteOfMarketClose = () => {
@@ -10,15 +9,5 @@ export const isWithinOneMinuteOfMarketClose = () => {
   const oneMinuteBeforeClose = new Date(nextClose.getTime() - 1000 * 60);
   const now = new Date();
   const withinOneMinuteOfClose = now > oneMinuteBeforeClose && now < nextClose;
-  console.log({
-    nextClose: getCST(nextClose),
-    oneMinuteBeforeClose: getCST(oneMinuteBeforeClose),
-    now: getCST(now),
-    withinOneMinuteOfClose,
-  });
-  if (!withinOneMinuteOfClose) {
-    console.log('Not within one minute of close, skipping dynamic stop loss listener.');
-    return;
-  }
-  console.log('Within one minute of close, moving forward with dynamic stop loss listener.');
+  return withinOneMinuteOfClose;
 };
